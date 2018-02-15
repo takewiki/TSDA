20e01e1430c0757065cc36d0574e10e5a3f14689
takewiki <hulilei@takewiki.com.cn>
2018-02-16 01:39:14 +0800

    mapping the functions from lubridate

---
+++ R/dateInput.R
@@ -0,0 +1,22 @@
+#' @import lubridate
+#' @export
+ymd <- ymd;
+#' @import lubridate
+#' @export
+yyyymmdd <-ymd;
+
+#' @import lubridate
+#' @export
+#' @example
+#' mdy("4/1/17")
+#' mmddyyyy('01-14-2018');
+mmddyyyy <-mdy;
+
+#' @import lubridate
+#' @export
+#' @example
+#' dmy("14/10/1979");
+ddmmyyyy <- dmy;
+
+
+
---
+++ data-raw/date_test.R
@@ -0,0 +1,14 @@
+library(tsda);
+cc <-tsda::ymd(20150112);
+class(cc);
+yyyymmdd(20180125);
+
+mmddyyyy("04/01/2017");
+
+ddmmyyyy('29/04/1984');
+
+
+time <- ymd_hms("2010-12-13 15:30:30")
+time
+
+class(time);
