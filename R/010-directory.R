20715de78acae18159d863f4e439c545e6278fa1
takewiki <hulilei@takewiki.com.cn>
2019-04-07 22:06:32 +0800

    add the skeleton files into the packages

---
+++ R/010-directory.R
@@ -0,0 +1,39 @@
+#' 显示目录下所有文件并打印出来，每个文件换行
+#'
+#' @param dir_path 目录路径
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples print_dir();
+print_dir <- function(dir_path) {
+  cat(paste(dir(dir_path),collapse = '\n'));
+}
+
+#' 获取某个文件夹的所有文件名
+#'
+#' @param dir_path 文件夹
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples dir_files();
+dir_files <- function(dir_path){
+  res <-dir(dir_path);
+  class(res) <- 'dir_files'
+  return(res);
+}
+
+#' 打印dir_files文件
+#'
+#' @param object  dir_files类型的对象
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples print();
+print.dir_files <- function(object){
+  object <- as.character(object);
+  cat(paste(object,collapse = '\n'));
+
+}
