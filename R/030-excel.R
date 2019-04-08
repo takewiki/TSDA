9fc1090dcd1bcb532e894eaf0abebcd42b2e6fd8
takewiki <hulilei@takewiki.com.cn>
2019-04-08 15:41:58 +0800

    delete merge sheets

--- R/030-excel.R
+++ R/030-excel.R
@@ -26,6 +26,25 @@ read_excelBook <- function(variables) {
 
 }
 
-read_excelBooks <- function(variables) {
-
+#' 将多个相同结构的EXCEL合并出一个数据
+#'
+#' @param dir_files excel_files所在的目录
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' read_excelBooks('./data-raw/ghdata')
+read_excelBooks <- function(dir_files) {
+  files <- dir(dir_files)
+  files <-paste(dir_files,files,sep="/");
+  #get files
+  res <-lapply(files, read_excelSheet)
+  #get names
+  field_names <-lapply(res,names)
+  #sheet name
+  sheet_header <- field_names[[1]];
+  res <- do.call("rbind",res);
+  names(res) <- sheet_header;
+  return(res);
 }
