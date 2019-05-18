429e26f7e940321b979535120deded7af417596e
takewiki <hulilei@takewiki.com.cn>
2019-05-18 16:55:24 +0800

    add the select template in sqlserver

---
+++ data-raw/DBMS/RJDBC/sqlserver/select_engine_sqlserver_template.R
@@ -0,0 +1,30 @@
+ select_engine_sqlserver(fieldName_vec_simple= ,
+ fieldName_caption_simple = NULL,
+  fieldName_vec_aggr = NULL,
+ fieldName_caption_aggr = NULL,
+ fun_vec_aggr = NULL,
+  table_name = ,
+ fieldName_vec_where = NULL,
+ comparerSig_vec_where = NULL,
+  filterValue_vec_where = NULL,
+ comboCondition_logi_vec_where = NULL,
+ fieldName_vec_groupBy = NULL,
+   fieldName_vec_orderBy = NULL,
+  asc_vec_orderBy = NULL)
+
+
+  select_engine_sqlserver(fieldName_vec_simple= ,
+ fieldName_caption_simple = NULL,
+  fieldName_vec_aggr = NULL,
+ fieldName_caption_aggr = NULL,
+ fun_vec_aggr = NULL,
+  table_name = ,
+ fieldName_vec_where = NULL,
+ comparerSig_vec_where = NULL,
+  filterValue_vec_where = NULL,
+ comboCondition_logi_vec_where = NULL,
+ fieldName_vec_groupBy = NULL,
+   fieldName_vec_orderBy = NULL,
+  asc_vec_orderBy = NULL)
+
+
