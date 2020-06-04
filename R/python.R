6ed571e5717ec28a5599c3a111c4c78266b80471
takewiki <hulilei@takewiki.com.cn>
2020-06-04 12:16:22 +0800

    upgrade into v3.1

---
+++ R/python.R
@@ -0,0 +1,15 @@
+#' 针对python环境进行设置
+#'
+#' @return 返回
+#' @export
+#'
+#' @examples
+#' set_py()
+set_py <- function() {
+  #不再使用连接的方式
+  #使用虚拟环境更容易对python包进行维护
+  use_virtualenv('/opt/my_env',required = TRUE)
+  res <- TRUE
+  return(res)
+
+}
